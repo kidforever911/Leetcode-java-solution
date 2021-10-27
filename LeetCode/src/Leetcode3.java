ba18bdcfaab5ca230889e86a33351c05c3a75523
12390 <pang_huadong0811@outlook.com>
2021-10-27 10:03:53 -0400

    改变格式

--- LeetCode/src/Leetcode3.java
+++ LeetCode/src/Leetcode3.java
@@ -1,48 +1,50 @@
 import java.util.HashMap;
 
-public class Leetcode3 {
-    class Solution {
-        public int lengthOfLongestSubstring(String s) {
-            if(s == null || s.isEmpty())
-                return 0;
 
-            int n = s.length();
-            char[] letters = s.toCharArray();
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.isEmpty())
+            return 0;
 
-            int fast = 0, slow = 0, max = 0;
+        int n = s.length();
+        char[] letters = s.toCharArray();
 
-            HashMap<Character, Integer> lastOccurrence = new HashMap<>();
+        int fast = 0, slow = 0, max = 0;
 
-            for (; fast < n; fast ++){
-                char curr = letters[fast];
-                if(lastOccurrence.containsKey(curr)){
-                    slow = Math.max(slow, lastOccurrence.get(curr) + 1);
-                }
-                lastOccurrence.put(curr, fast);
-                max = Math.max(max, fast - slow + 1);
+        HashMap<Character, Integer> lastOccurrence = new HashMap<>();
+
+        for (; fast < n; fast++) {
+            char curr = letters[fast];
+            if (lastOccurrence.containsKey(curr)) {
+                slow = Math.max(slow, lastOccurrence.get(curr) + 1);
             }
-            return max;
+            lastOccurrence.put(curr, fast);
+            max = Math.max(max, fast - slow + 1);
         }
-    }
-
-    class Solution {
-        public int lengthOfLongestSubstring(String s) {
-            int[] freq = new int[256];
-            int left = 0;
-            int right = -1;
-            char[] letters = s.toCharArray();
-            int max = 0;
-            while(left < letters.length){
-                if(right + 1 < letters.length && freq[letters[right + 1]] == 0){
-                    right ++;
-                    freq[letters[right]]++;
-                }else{
-                    freq[letters[left]]--;
-                    left++;
-                }
-                max = Math.max(max, right - left + 1);
-            }
-            return max;
-        }
+        return max;
     }
 }
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int[] freq = new int[256];
+        int left = 0;
+        int right = -1;
+        char[] letters = s.toCharArray();
+        int max = 0;
+        while (left < letters.length) {
+            if (right + 1 < letters.length && freq[letters[right + 1]] == 0) {
+                right++;
+                freq[letters[right]]++;
+            } else {
+                freq[letters[left]]--;
+                left++;
+            }
+            max = Math.max(max, right - left + 1);
+        }
+        return max;
+    }
+}
+
+
+
