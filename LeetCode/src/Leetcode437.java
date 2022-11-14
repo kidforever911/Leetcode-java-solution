628351ca757309fed769cc24fae833688cc6985d
kidforever <pang_huadong0811@outlook.com>
2022-11-13 21:44:38 -0500

    新增：Leetcode 修改：Leetcode437,447,450

--- LeetCode/src/Leetcode437.java
+++ LeetCode/src/Leetcode437.java
@@ -1,21 +1,21 @@
 import javax.swing.tree.TreeNode;
 
-public class Leetcode437 {
-    class Solution {
-        public int pathSum(TreeNode root, int targetSum) {
-            if(root == null) return 0;
-            int result = findPath(root, targetSum);
-            result += pathSum(root.left, targetSum);
-            result += pathSum(root.right, targetSum);
-            return result;
-        }
-        private int findPath(TreeNode root, int sum){
-            if(root == null) return 0;
-            int res = 0;
-            if(root.val == sum) res += 1;
-            res += findPath(root.left, sum - root.val);
-            res += findPath(root.right, sum - root.val);
-            return res;
-        }
+class Solution {
+    public int pathSum(TreeNode root, int targetSum) {
+        if (root == null) return 0;
+        int result = findPath(root, targetSum);
+        result += pathSum(root.left, targetSum);
+        result += pathSum(root.right, targetSum);
+        return result;
+    }
+
+    private int findPath(TreeNode root, int sum) {
+        if (root == null) return 0;
+        int res = 0;
+        if (root.val == sum) res += 1;
+        res += findPath(root.left, sum - root.val);
+        res += findPath(root.right, sum - root.val);
+        return res;
     }
 }
+
--- LeetCode/src/Leetcode447.java
+++ LeetCode/src/Leetcode447.java
@@ -1,31 +1,30 @@
 import java.util.HashMap;
 
-public class Leetcode447 {
-    class Solution {
-        public int numberOfBoomerangs(int[][] points) {
-            int result = 0;
-            if(points.length == 0) return result;
-            HashMap<Integer, Integer> record = new HashMap<>();
-            for(int i = 0; i < points.length; i++){
-                for(int j = 0; j < points.length; j++){
-                    if(i == j){
-                        continue;
-                    }
-                    int dist = getDistance(points[i], points[j]);
-                    record.put(dist, record.getOrDefault(dist, 0) + 1);
+class Solution {
+    public int numberOfBoomerangs(int[][] points) {
+        int result = 0;
+        if (points.length == 0) return result;
+        HashMap<Integer, Integer> record = new HashMap<>();
+        for (int i = 0; i < points.length; i++) {
+            for (int j = 0; j < points.length; j++) {
+                if (i == j) {
+                    continue;
                 }
-                for(int val : record.values()){
-                    result += val * (val - 1);
-                }
-                record.clear();
+                int dist = getDistance(points[i], points[j]);
+                record.put(dist, record.getOrDefault(dist, 0) + 1);
             }
-            return result;
+            for (int val : record.values()) {
+                result += val * (val - 1);
+            }
+            record.clear();
         }
+        return result;
+    }
 
-        private int getDistance(int[] a, int[] b){
-            int dx = a[0] - b[0];
-            int dy = a[1] - b[1];
-            return dx*dx + dy*dy;
-        }
+    private int getDistance(int[] a, int[] b) {
+        int dx = a[0] - b[0];
+        int dy = a[1] - b[1];
+        return dx * dx + dy * dy;
     }
 }
+
